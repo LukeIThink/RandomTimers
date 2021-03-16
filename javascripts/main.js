d89ad759f24e99a59e7b12d43cf1ae52bead0873
LukeIThink <62862211+LukeIThink@users.noreply.github.com>
2021-03-16 15:27:56 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -16,7 +16,7 @@ function getSec() {
 function getM() {
 	var today = new Date();
 	var hour = today.getHours() - (today.getHours() < 13 ? 0 : 24);
-	return(getHour() <= 12 ? "A" : "P");
+	return(hour <= 12 ? "A" : "P");
 };
 
 function getTime() {
